40f7ff35fee742ba569b99ef17ecd85ceb4c4c83
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-05-16 19:45:11 -0500

    metodo restar

--- Calculadora.java
+++ Calculadora.java
@@ -12,6 +12,21 @@ public class Calculadora{
 		System.out.println("Escoja la operacion:"+"\n"+"1. Suma"+"\n"+"2. Resta"+"\n"+
 				"3. Multiplicacion"+"\n"+"4. Division"+"\n"+"5. Modulo");
 		op=sc.nextInt();
+		switch(op){
+			case 1:
+				res=1;
+			case 2:
+				restar(num1,num2);
+			case 3:
+				res=3;
+			case 4:
+				res=4;
+			case 5:
+				res=5;
+		}
 		System.out.println("El resultado es:"+res);
+	}
+	public int restar(int num1,int num2){
+		return a-b;
 	}
 }
