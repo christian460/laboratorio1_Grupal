e8c90daabc4021d10f548c15556873681f1cdac3
Joshep-c <121264024+Joshep-c@users.noreply.github.com>
2023-05-18 15:48:31 -0500

    Presentacion final.java

--- Calculadora.java
+++ Calculadora.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 public class Calculadora{
 	public static void main (String arg[]){
-		int num1=0,num2=0,res=0,op=0;
 		Scanner sc=new Scanner(System.in);
+		
+		int num1=0,num2=0,res=0,op=0;
+		
 		System.out.println("Calculadora");
 		System.out.println("Ingrese primer numero:");
 		num1=sc.nextInt();
 		System.out.println("Ingrese segundo numero:");
 		num2=sc.nextInt();
+		
 		System.out.println("Escoja la operacion:"+"\n"+"1. Suma"+"\n"+"2. Resta"+"\n"+
 				"3. Multiplicacion"+"\n"+"4. Division"+"\n"+"5. Modulo");
 		op=sc.nextInt();
@@ -32,23 +35,23 @@ public class Calculadora{
 		System.out.println("El resultado es:" + res);
 	}
 
-	public static int add(int num1,int num2){
+	public static int add(int num1, int num2){
 		return num1 + num2;
 	}
 	
-	public static int restar(int num1,int num2){
+	public static int restar(int num1, int num2){
 		return num1 - num2;
 	}
 	
-	public static int mul(int num1,int num2){
+	public static int mul(int num1, int num2){
 		return num1 * num2;
 	}
 	
-	public static int dividir(int num1,int num2){
+	public static int dividir(int num1, int num2){
 		return num1 / num2;
 	}
 	
-	public static int mod(int num1,int num2){
+	public static int mod(int num1, int num2){
 		return num1 % num2;
 	}
 }
